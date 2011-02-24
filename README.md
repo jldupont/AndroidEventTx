55186efd7e4d0a1805d9a29eeceb507c28b58bb9
jldupont <github@jldupont.com>
2011-02-23 21:29:27 -0500

    1st

---
+++ README.md
@@ -0,0 +1,5 @@
+AndroidEventTx
+==============
+
+Android Event Transmitter
+
---
+++ src/com/systemical/system/MsgSwitch.java
@@ -0,0 +1,12 @@
+/**
+ * MsgSwitch - Inter-thread Message Switch
+ * 
+ * @author jldupont
+ */
+package com.systemical.system;
+
+public class MsgSwitch {
+
+	
+	
+}//class
